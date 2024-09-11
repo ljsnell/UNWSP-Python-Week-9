f64f745ca106f911c4e5b868936f8289e1f82d59
ljsnell <ljsnell7@gmail.com>
2024-09-10 19:47:44 -0500

    Updated program comments.

---
+++ program_1.py
@@ -0,0 +1,3 @@
+# Program #1: Item Counter
+#  Assume a file containing a series of names (as strings) is named names.txt and exists on the computer's disk.
+# Write a program that displays the number of names that are stored in the file.
---
+++ program_3.py
@@ -0,0 +1,8 @@
+# Program #3: Average Numbers
+# Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk. 
+# Write a program that reads all of the numbers stored in the file and calculates their total.  
+
+# The program should handle the following exceptions: 
+
+# It should handle any IOError exceptions that are raised.
+# It should handle any ValueError exceptions that are raised when the items that are read from the file are converted to a number.
